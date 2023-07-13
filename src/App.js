c2571bedb96e4a57b42a31001b9f06aad5a42d19
TimoteoGambella <timi.gambella@hotmail.com>
2023-07-13 20:19:42 -0300

    errorScreen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { ApiContext } from './context/ApiContext';
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
+import { useMediaQuery } from '@mui/material';
 
 function App() {
+  const is360screen=useMediaQuery("(min-width:360px)")
+
   return (
-    <div className='mobileContainer'>
-      <ApiContext>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-            </Routes>
-          </Router>
-      </ApiContext>
-    </div>
+    <>
+    {is360screen ? 
+      <div className='mobileContainer'>
+        <ApiContext>
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </Router>
+        </ApiContext>
+      </div>
+      :
+      <div className='errorScreen'>
+        <p>Este sitio está disponible sólo para pantallas 360px o mayores</p>
+        <p>This site is available only for screens of 360px or higher</p>
+      </div>
+    }
+    </>
   );
 }
 
